828501bb233d9e4c5d60a31b5b181778349d05df
Kaan Keskinsoy <kk148@hdm-stuttgart.de>
2020-06-16 13:16:16 +0200

    Document Code.

--- models/disparity_adjustment.py
+++ models/disparity_adjustment.py
@@ -12,22 +12,22 @@ def disparity_adjustment(tenImage, tenDisparity):
 
 	objPredictions = moduleMaskrcnn([ tenImage[ 0, [ 2, 0, 1 ], :, : ] ])[0]
 
-	for intMask in range(objPredictions['masks'].shape[0]):
+	for intMask in range(objPredictions['masks'].shape[0]): # objPredictions['masks'].shape = [4, 1, 768, 1024]
 		if intMask in boolUsed:
 			continue
 
 		elif objPredictions['scores'][intMask].item() < 0.7:
 			continue
 
-		elif objPredictions['labels'][intMask].item() not in [ 1, 3, 6, 7, 8, 9, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ]:
+		elif objPredictions['labels'][intMask].item() not in [ 1, 3, 6, 7, 8, 9, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ]: # specific object detected: humans, cars, animals, etc.
 			continue
 
 		# end
 
-		boolUsed[intMask] = True
-		tenMask = (objPredictions['masks'][(intMask + 0):(intMask + 1), :, :, :] > 0.5).float()
+		boolUsed[intMask] = True # if mask is of specific object => add to adjustment todo list
+		tenMask = (objPredictions['masks'][(intMask + 0):(intMask + 1), :, :, :] > 0.5).float() # add threshold to selected mask => 0 or 1 values for mask
 
-		if tenMask.sum().item() < 64:
+		if tenMask.sum().item() < 64: # ignore small masks
 			continue
 		# end
 
@@ -38,28 +38,28 @@ def disparity_adjustment(tenImage, tenDisparity):
 			elif objPredictions['scores'][intMerge].item() < 0.7:
 				continue
 
-			elif objPredictions['labels'][intMerge].item() not in [ 2, 4, 27, 28, 31, 32, 33 ]:
+			elif objPredictions['labels'][intMerge].item() not in [ 2, 4, 27, 28, 31, 32, 33 ]: # specific object detected
 				continue
 
 			# end
 
-			tenMerge = (objPredictions['masks'][(intMerge + 0):(intMerge + 1), :, :, :] > 0.5).float()
+			tenMerge = (objPredictions['masks'][(intMerge + 0):(intMerge + 1), :, :, :] > 0.5).float() # add threshold to selected mask => 0 or 1 values for mask
 
-			if ((tenMask + tenMerge) > 1.0).sum().item() < 0.03 * tenMerge.sum().item():
+			if ((tenMask + tenMerge) > 1.0).sum().item() < 0.03 * tenMerge.sum().item(): # ???
 				continue
 			# end
 
 			boolUsed[intMerge] = True
-			tenMask = (tenMask + tenMerge).clamp(0.0, 1.0)
+			tenMask = (tenMask + tenMerge).clamp(0.0, 1.0) # set values < 0 to 0.0 # set values > 1 to 1.0
 		# end
 
 		tenMasks.append(tenMask)
 	# end
 
-	tenAdjusted = torch.nn.functional.interpolate(input=tenDisparity, size=(tenImage.shape[2], tenImage.shape[3]), mode='bilinear', align_corners=False)
+	tenAdjusted = torch.nn.functional.interpolate(input=tenDisparity, size=(tenImage.shape[2], tenImage.shape[3]), mode='bilinear', align_corners=False) # Resize tenDisparity to original tenImage size
 
 	for tenAdjust in tenMasks:
-		tenPlane = tenAdjusted * tenAdjust
+		tenPlane = tenAdjusted * tenAdjust # ???
 
 		tenPlane = torch.nn.functional.max_pool2d(input=tenPlane.neg(), kernel_size=3, stride=1, padding=1).neg()
 		tenPlane = torch.nn.functional.max_pool2d(input=tenPlane.neg(), kernel_size=3, stride=1, padding=1).neg()
--- models/disparity_estimation.py
+++ models/disparity_estimation.py
@@ -15,9 +15,9 @@ class Basic(torch.nn.Module):
 
 		elif strType == 'conv-relu-conv':
 			self.moduleMain = torch.nn.Sequential(
-				torch.nn.Conv2d(in_channels=intChannels[0], out_channels=intChannels[1], kernel_size=3, stride=1, padding=1),
-				torch.nn.PReLU(num_parameters=intChannels[1], init=0.25),
-				torch.nn.Conv2d(in_channels=intChannels[1], out_channels=intChannels[2], kernel_size=3, stride=1, padding=1)
+				torch.nn.Conv2d(in_channels=intChannels[0], out_channels=intChannels[1], kernel_size=3, stride=1, padding=1), # in_channels=32, out_channels=32
+				torch.nn.PReLU(num_parameters=intChannels[1], init=0.25), # num_parameters=32
+				torch.nn.Conv2d(in_channels=intChannels[1], out_channels=intChannels[2], kernel_size=3, stride=1, padding=1) # in_channels=32, out_channels=1
 			)
 
 		# end
@@ -26,7 +26,7 @@ class Basic(torch.nn.Module):
 			self.moduleShortcut = None
 
 		elif intChannels[0] != intChannels[2]:
-			self.moduleShortcut = torch.nn.Conv2d(in_channels=intChannels[0], out_channels=intChannels[2], kernel_size=1, stride=1, padding=0)
+			self.moduleShortcut = torch.nn.Conv2d(in_channels=intChannels[0], out_channels=intChannels[2], kernel_size=1, stride=1, padding=0) # in_channels=32, out_channels=1
 
 		# end
 	# end
@@ -104,8 +104,10 @@ class Semantics(torch.nn.Module):
 	# end
 
 	def forward(self, tenInput):
+		# Reverse order of channel
 		tenPreprocessed = tenInput[:, [ 2, 1, 0 ], :, :]
 
+		# Preprocessing ???
 		tenPreprocessed[:, 0, :, :] = (tenPreprocessed[:, 0, :, :] - 0.485) / 0.229
 		tenPreprocessed[:, 1, :, :] = (tenPreprocessed[:, 1, :, :] - 0.456) / 0.224
 		tenPreprocessed[:, 2, :, :] = (tenPreprocessed[:, 2, :, :] - 0.406) / 0.225
@@ -121,26 +123,28 @@ class Disparity(torch.nn.Module):
 		self.moduleImage = torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=7, stride=2, padding=3)
 		self.moduleSemantics = torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
 
+		# Three 512 => semantic features
+		# First three 32, 48, 64 => image information
 		for intRow, intFeatures in [ (0, 32), (1, 48), (2, 64), (3, 512), (4, 512), (5, 512) ]:
-			self.add_module(str(intRow) + 'x0' + ' - ' + str(intRow) + 'x1', Basic('relu-conv-relu-conv', [ intFeatures, intFeatures, intFeatures ]))
-			self.add_module(str(intRow) + 'x1' + ' - ' + str(intRow) + 'x2', Basic('relu-conv-relu-conv', [ intFeatures, intFeatures, intFeatures ]))
-			self.add_module(str(intRow) + 'x2' + ' - ' + str(intRow) + 'x3', Basic('relu-conv-relu-conv', [ intFeatures, intFeatures, intFeatures ]))
+			self.add_module(str(intRow) + 'x0' + ' - ' + str(intRow) + 'x1', Basic('relu-conv-relu-conv', [ intFeatures, intFeatures, intFeatures ])) # '0x0 - 0x1', '1x0 - 1x1', ..., '5x0 - 5x1'
+			self.add_module(str(intRow) + 'x1' + ' - ' + str(intRow) + 'x2', Basic('relu-conv-relu-conv', [ intFeatures, intFeatures, intFeatures ])) # '0x1 - 0x2', '1x1 - 1x2', ..., '5x1 - 5x2'
+			self.add_module(str(intRow) + 'x2' + ' - ' + str(intRow) + 'x3', Basic('relu-conv-relu-conv', [ intFeatures, intFeatures, intFeatures ])) # '0x2 - 0x3', '1x2 - 1x3', ..., '5x2 - 5x3'
 		# end
 
 		for intCol in [ 0, 1 ]:
-			self.add_module('0x' + str(intCol) + ' - ' + '1x' + str(intCol), Downsample([ 32, 48, 48 ]))
-			self.add_module('1x' + str(intCol) + ' - ' + '2x' + str(intCol), Downsample([ 48, 64, 64 ]))
-			self.add_module('2x' + str(intCol) + ' - ' + '3x' + str(intCol), Downsample([ 64, 512, 512 ]))
-			self.add_module('3x' + str(intCol) + ' - ' + '4x' + str(intCol), Downsample([ 512, 512, 512 ]))
-			self.add_module('4x' + str(intCol) + ' - ' + '5x' + str(intCol), Downsample([ 512, 512, 512 ]))
+			self.add_module('0x' + str(intCol) + ' - ' + '1x' + str(intCol), Downsample([ 32, 48, 48 ])) # '0x0 - 1x0', '0x1 - 1x1'
+			self.add_module('1x' + str(intCol) + ' - ' + '2x' + str(intCol), Downsample([ 48, 64, 64 ])) # '1x0 - 2x0', '1x1 - 2x1'
+			self.add_module('2x' + str(intCol) + ' - ' + '3x' + str(intCol), Downsample([ 64, 512, 512 ])) # '2x0 - 3x0', '2x1 - 3x1'
+			self.add_module('3x' + str(intCol) + ' - ' + '4x' + str(intCol), Downsample([ 512, 512, 512 ])) # '3x0 - 4x0', '3x1 - 4x1'
+			self.add_module('4x' + str(intCol) + ' - ' + '5x' + str(intCol), Downsample([ 512, 512, 512 ])) # '4x0 - 5x0', '4x1 - 5x1'
 		# end
 
 		for intCol in [ 2, 3 ]:
-			self.add_module('5x' + str(intCol) + ' - ' + '4x' + str(intCol), Upsample([ 512, 512, 512 ]))
-			self.add_module('4x' + str(intCol) + ' - ' + '3x' + str(intCol), Upsample([ 512, 512, 512 ]))
-			self.add_module('3x' + str(intCol) + ' - ' + '2x' + str(intCol), Upsample([ 512, 64, 64 ]))
-			self.add_module('2x' + str(intCol) + ' - ' + '1x' + str(intCol), Upsample([ 64, 48, 48 ]))
-			self.add_module('1x' + str(intCol) + ' - ' + '0x' + str(intCol), Upsample([ 48, 32, 32 ]))
+			self.add_module('5x' + str(intCol) + ' - ' + '4x' + str(intCol), Upsample([ 512, 512, 512 ])) # '5x2 - 4x2', '5x3 - 4x3'
+			self.add_module('4x' + str(intCol) + ' - ' + '3x' + str(intCol), Upsample([ 512, 512, 512 ])) # '4x2 - 3x2', '4x3 - 3x3'
+			self.add_module('3x' + str(intCol) + ' - ' + '2x' + str(intCol), Upsample([ 512, 64, 64 ])) # '3x2 - 2x2', '3x3 - 2x3'
+			self.add_module('2x' + str(intCol) + ' - ' + '1x' + str(intCol), Upsample([ 64, 48, 48 ])) # '2x2 - 1x2', , '2x3 - 1x3'
+			self.add_module('1x' + str(intCol) + ' - ' + '0x' + str(intCol), Upsample([ 48, 32, 32 ])) # '1x2 - 0x2', '1x3 - 0x3'
 		# end
 
 		self.moduleDisparity = Basic('conv-relu-conv', [ 32, 32, 1 ])
@@ -150,27 +154,27 @@ class Disparity(torch.nn.Module):
 		tenColumn = [ None, None, None, None, None, None ]
 
 		tenColumn[0] = self.moduleImage(tenImage)
-		tenColumn[1] = self._modules['0x0 - 1x0'](tenColumn[0])
-		tenColumn[2] = self._modules['1x0 - 2x0'](tenColumn[1])
+		tenColumn[1] = self._modules['0x0 - 1x0'](tenColumn[0]) # Downsample ...
+		tenColumn[2] = self._modules['1x0 - 2x0'](tenColumn[1]) 
 		tenColumn[3] = self._modules['2x0 - 3x0'](tenColumn[2]) + self.moduleSemantics(tenSemantics)
 		tenColumn[4] = self._modules['3x0 - 4x0'](tenColumn[3])
-		tenColumn[5] = self._modules['4x0 - 5x0'](tenColumn[4])
+		tenColumn[5] = self._modules['4x0 - 5x0'](tenColumn[4]) # ... Downsample
 
 		intColumn = 1
 		for intRow in range(len(tenColumn)):
-			tenColumn[intRow] = self._modules[str(intRow) + 'x' + str(intColumn - 1) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow])
-			if intRow != 0:
-				tenColumn[intRow] += self._modules[str(intRow - 1) + 'x' + str(intColumn) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow - 1])
+			tenColumn[intRow] = self._modules[str(intRow) + 'x' + str(intColumn - 1) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow]) # '0x0 - 0x1', '1x0 - 1x1', ..., '5x0 - 5x1' # Image and semantic features
+			if intRow != 0: # ignore first
+				tenColumn[intRow] += self._modules[str(intRow - 1) + 'x' + str(intColumn) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow - 1]) # '0x1 - 1x1', '1x1 - 2x1', '2x1 - 3x1', '3x1 - 4x1', '4x1 - 5x1' # Downsampling col 1
 			# end
 		# end
 
 		intColumn = 2
 		for intRow in range(len(tenColumn) -1, -1, -1):
-			tenColumn[intRow] = self._modules[str(intRow) + 'x' + str(intColumn - 1) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow])
-			if intRow != len(tenColumn) - 1:
-				tenUp = self._modules[str(intRow + 1) + 'x' + str(intColumn) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow + 1])
+			tenColumn[intRow] = self._modules[str(intRow) + 'x' + str(intColumn - 1) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow]) # '5x1 - 5x2', ..., '1x1 - 1x2', '0x1 - 0x2'
+			if intRow != len(tenColumn) - 1: # ignore first
+				tenUp = self._modules[str(intRow + 1) + 'x' + str(intColumn) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow + 1]) # '5x2 - 4x2', '4x2 - 3x2', '3x2 - 2x2', '2x2 - 1x2', '1x2 - 0x2' # Upsampline col 2
 
-				if tenUp.shape[2] != tenColumn[intRow].shape[2]: tenUp = torch.nn.functional.pad(input=tenUp, pad=[ 0, 0, 0, -1 ], mode='constant', value=0.0)
+				if tenUp.shape[2] != tenColumn[intRow].shape[2]: tenUp = torch.nn.functional.pad(input=tenUp, pad=[ 0, 0, 0, -1 ], mode='constant', value=0.0) # padding
 				if tenUp.shape[3] != tenColumn[intRow].shape[3]: tenUp = torch.nn.functional.pad(input=tenUp, pad=[ 0, -1, 0, 0 ], mode='constant', value=0.0)
 
 				tenColumn[intRow] += tenUp
@@ -179,11 +183,11 @@ class Disparity(torch.nn.Module):
 
 		intColumn = 3
 		for intRow in range(len(tenColumn) -1, -1, -1):
-			tenColumn[intRow] = self._modules[str(intRow) + 'x' + str(intColumn - 1) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow])
-			if intRow != len(tenColumn) - 1:
-				tenUp = self._modules[str(intRow + 1) + 'x' + str(intColumn) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow + 1])
+			tenColumn[intRow] = self._modules[str(intRow) + 'x' + str(intColumn - 1) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow]) # '5x2 - 5x3', ..., '1x2 - 1x3', '0x2 - 0x3'  
+			if intRow != len(tenColumn) - 1: # ignore first
+				tenUp = self._modules[str(intRow + 1) + 'x' + str(intColumn) + ' - ' + str(intRow) + 'x' + str(intColumn)](tenColumn[intRow + 1]) # '5x3 - 4x3', '4x3 - 3x3', '3x3 - 2x3', '2x3 - 1x3', '1x3 - 0x3' # Upsampline col 3
 
-				if tenUp.shape[2] != tenColumn[intRow].shape[2]: tenUp = torch.nn.functional.pad(input=tenUp, pad=[ 0, 0, 0, -1 ], mode='constant', value=0.0)
+				if tenUp.shape[2] != tenColumn[intRow].shape[2]: tenUp = torch.nn.functional.pad(input=tenUp, pad=[ 0, 0, 0, -1 ], mode='constant', value=0.0) # padding
 				if tenUp.shape[3] != tenColumn[intRow].shape[3]: tenUp = torch.nn.functional.pad(input=tenUp, pad=[ 0, -1, 0, 0 ], mode='constant', value=0.0)
 
 				tenColumn[intRow] += tenUp
@@ -214,5 +218,6 @@ def disparity_estimation(tenImage):
 	# and the interpolation uses edge value padding for out-of-boundary values
 	tenImage = torch.nn.functional.interpolate(input=tenImage, size=(intHeight, intWidth), mode='bilinear', align_corners=False)
 
+	# 1. call Semantics().forward() 2. call Disparity().forward()
 	return moduleDisparity(tenImage, moduleSemantics(tenImage))
 # end
