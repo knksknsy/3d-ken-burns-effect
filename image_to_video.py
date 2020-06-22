bf5aa4c6623fb6fa155098191eea260dd5c64ed6
Kaan Keskinsoy <kk148@hdm-stuttgart.de>
2020-06-22 19:31:23 +0200

    Add path argument to image_to_video script.

--- image_to_video.py
+++ image_to_video.py
@@ -1,5 +1,6 @@
 '''Generate a video for each data (stereo-vision pictures) in 3d-ken-burns-dataset'''
 '''Place this script into the 3d-ken-burns-dataset directory and execute it in order to generate videos'''
+'''Usage e.g.: python image_to_video.py --path D:\3d-ken-burns-dataset'''
 
 from cv2 import cv2
 from tqdm import tqdm
@@ -7,31 +8,35 @@ import os
 import numpy
 import sys
 from zipfile import ZipFile
+from pathlib import Path
+import getopt
 
 class Images2Video:
-    def __init__(self, data_path):
+    def __init__(self, data_path, pwd):
         self.data_path = data_path
-        self.output_path = 'videos'
+        self.pwd = pwd
+        self.output_path = Path(os.path.join(self.pwd, 'videos'))
 
         if not os.path.exists(self.output_path):
             os.mkdir(self.output_path)
 
     def generate(self):
-        video_name = self.data_path.replace('.zip', '') + '.avi'
-        video_path = self.output_path + video_name
+        video_name = str(self.data_path).split(os.path.sep)[-1].replace('.zip', '.avi')
+        video_path = Path(os.path.join(self.output_path,video_name))
+        print(f'video_path: {video_path}')
 
         if os.path.exists(video_path):
             print(f'Skip gererating duplicate {video_name}')
             return
-        print(f'Generating {video_name}...')
+        print(f'Generating {video_name}')
 
         images = [img for img in ZipFile(self.data_path).namelist() if img.endswith('.png')]
         images_exr = [img for img in ZipFile(self.data_path).namelist() if img.endswith('.exr')]
 
         if len(images) > 0:
-            self.process_image(images, video_name, video_path)
+            self.process_image(images, video_name, str(video_path))
         if len(images_exr) > 0:
-            self.process_exr(images_exr, video_name, video_path)
+            self.process_exr(images_exr, video_name, str(video_path))
 
     def process_image(self, images, video_name, video_path):
         archive = ZipFile(self.data_path, 'r')
@@ -57,7 +62,7 @@ class Images2Video:
             image_tr = cv2.imdecode(numpy.frombuffer(image_tr_data, numpy.uint8), cv2.IMREAD_COLOR)
 
             image_stitched = self.stitch_images(image_tl, image_tr, image_bl, image_br, shape=(height, width))
-            video.write(image)
+            video.write(image_stitched)
 
         cv2.destroyAllWindows()
         video.release()
@@ -124,30 +129,43 @@ def select_dataset(dirs, pwd):
     dataset = []
 
     if data_path in dirs:
-        dataset.append(pwd + data_path)
+        dataset.append(os.path.join(pwd, data_path))
     elif data_path == 'all':
-        dataset = [pwd + d for d in dirs]
+        dataset = [os.path.join(pwd, d) for d in dirs]
     elif data_path == '--color':
-        dataset = [pwd + d for d in dirs if '-depth' not in d]
+        dataset = [os.path.join(pwd, d) for d in dirs if '-depth' not in d]
     elif data_path == '--depth':
-        dataset = [pwd + d for d in dirs if '-depth' in d]
+        dataset = [os.path.join(pwd, d) for d in dirs if '-depth' in d]
     else:
         print('Dataset not found.')
         return select_dataset(dirs, pwd)
+
+    # Convert path strings to path object
+    dataset = [Path(d) for d in dataset]
     return dataset
 
 def main():
-    pwd = './'
-    dirs = [d for d in os.listdir(pwd) if d.endswith('.zip')] #or os.path.isdir(os.path.join(pwd, d))]
+    arguments_path = './'
+
+    for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
+	    if strOption == '--path' and strArgument != '': arguments_path = strArgument # path to the datasets
+
+    dirs = [d for d in os.listdir(arguments_path) if d.endswith('.zip')] #or os.path.isdir(os.path.join(pwd, d))]
 
     print('Available dataset:')
     for d in dirs:
         print(f'\t- {d}')
 
-    selected_data = select_dataset(dirs, pwd)
+    selected_data = select_dataset(dirs, Path(arguments_path))
+
+    '''
+    print('Selected datasets:')
+    for sd in selected_data:
+        print(f'\t{sd}')
+    '''
 
     for sd in selected_data:
-        img2vid = Images2Video(sd)
+        img2vid = Images2Video(sd, Path(arguments_path))
         img2vid.generate()
 
 if __name__ == "__main__":
