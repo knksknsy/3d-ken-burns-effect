9f8ad55c7296961cb7e0c9679b5f2c9754d4e8eb
Kaan Keskinsoy <kk148@hdm-stuttgart.de>
2020-07-01 17:02:15 +0200

    Update dataset.py script to generate csv file containing dataset meta information (zip paths, image/exr paths, fov).

--- dataset.py
+++ dataset.py
@@ -9,6 +9,10 @@ import os.path
 from os import path
 from pathlib import Path
 import getopt
+from zipfile import ZipFile
+import pandas as pd
+from tqdm import tqdm
+import json
 
 class Category(Enum):
     SCENE = 0
@@ -21,16 +25,9 @@ class Category(Enum):
     URL_COLOR = 7
     URL_DEPTH = 8
 
-def main():
-    dataset_dir_name = '3d-ken-burns-dataset'
-    dataset_path = './'
+def get_dataset():
     dataset_txt = "dataset.txt"
     dataset = []
-
-    for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
-	    if strOption == '--path' and strArgument != '': dataset_path = strArgument # path to the datasets
-
-    dataset_path = os.path.join(dataset_path, dataset_dir_name)
 
     # Read dataset.txt file
     with open(dataset_txt) as f:
@@ -72,20 +69,10 @@ def main():
 
         dataset[i].append(url_color.group(1))
         dataset[i].append(url_depth.group(1))
-
-    print(f'Total dataset size: {np.sum(filesizes)} GB')
-
-    # Create 3d-ken-burns-dataset directory if it does not exist
-    if not path.exists(dataset_path):
-        os.mkdir(dataset_path)
     
-    # User input for starting or canceling download procedure
-    print('Start download? Type [N] to cancel. Press return to start download.')
-    proceed = input()
-    if proceed == 'N':
-        print('Download terminated.')
-        sys.exit()
+    return dataset, filesizes
 
+def download_dataset(dataset):
     # Start download
     for i, data in enumerate(dataset):
         # Extract meta-information
@@ -113,5 +100,98 @@ def main():
             url_depth = data[Category.URL_DEPTH.value]
             urllib.request.urlretrieve(url_depth, file_path_depth)
 
+def main(dataset_path):
+    dataset, filesizes = get_dataset()
+    print(f'Total dataset size: {np.sum(filesizes)} GB')
+
+    # Create 3d-ken-burns-dataset directory if it does not exist
+    if not path.exists(dataset_path):
+        os.mkdir(dataset_path)
+    
+    # User input for starting or canceling download procedure
+    print('Start download? Type [N] to cancel. Press return to start download.')
+    proceed = input()
+    if proceed == 'N':
+        print('Download terminated.')
+        sys.exit()
+
+    download_dataset(dataset)
+    print(f'Datasets downloaded into path: {dataset_path}')
+
+def create_csv(dataset_path):
+    print('Creating CVS...')
+    dataset_zip_color_paths = [f for f in os.listdir(dataset_path) if f.endswith('.zip') and '-depth' not in f]
+    dataset_zip_depth_paths = [f for f in os.listdir(dataset_path) if f.endswith('.zip') and '-depth' in f]
+
+    dataset_zip_color_paths.sort()
+    dataset_zip_depth_paths.sort()
+
+    df_columns = ['zip_image_path', 'zip_depth_path', 'image_path', 'depth_path', 'fltFov']
+
+    for i, (zip_color_path, zip_depth_path) in enumerate(zip(dataset_zip_color_paths, dataset_zip_depth_paths)):
+        # Data frame containing information to a single dataset
+        data_frame = pd.DataFrame(columns=df_columns)
+
+        absolute_zip_color_path = os.path.join(dataset_path, zip_color_path)
+        absolute_zip_depth_path = os.path.join(dataset_path, zip_depth_path)
+
+        image_color_paths = [image for image in ZipFile(absolute_zip_color_path).namelist() if image.endswith('.png')]
+        image_color_paths = np.array(image_color_paths, dtype=object)
+
+        image_depth_paths = [depth for depth in ZipFile(absolute_zip_depth_path).namelist() if depth.endswith('.exr')]
+        image_depth_paths = np.array(image_depth_paths, dtype=object)
+
+        meta_paths = [meta for meta in ZipFile(absolute_zip_color_path).namelist() if meta.endswith('.json')]
+        meta_paths = np.array(meta_paths, dtype=object)
+        # Extend meta_paths to length of image_color_paths/image_depth_paths
+        meta_paths = np.tile(meta_paths, 4)
+        meta_paths = np.sort(meta_paths)
+
+        # Read json attribute 'fltFov' from meta file
+        meta_archive = ZipFile(absolute_zip_color_path, 'r')
+        for j, (m1, m2, m3, m4) in enumerate(zip(*[iter(meta_paths)]*4)):
+            meta_json = meta_archive.read(m1).decode("utf-8")
+            meta_json = json.loads(meta_json)
+            meta_paths[j*4:(j*4)+4] = float(meta_json['fltFov'])
+
+        progress_bar = tqdm(zip(image_color_paths, image_depth_paths, meta_paths))
+        for j, (c, d, m) in enumerate(progress_bar):
+            progress_bar.set_description(f'Preparing CSV for {zip_color_path}: {j+1}/{len(image_color_paths)+1}')
+            series = pd.Series([zip_color_path, zip_depth_path, c, d, m], index=df_columns)
+            data_frame = data_frame.append(series, ignore_index=True)
+
+        # Create CSV
+        data_frame.to_csv(f'dataset_{zip_color_path}.csv', index=False, encoding='utf-8')
+
+    # Concat every CSV file
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    csv_paths = [f for f in os.listdir(pwd) if f.endswith('.csv')]
+    # Data frame containing information for all datasets
+    data_frames = []
+    for f in csv_paths:
+        data_frame = pd.read_csv(f, index_col=None, header=0)
+        data_frames.append(data_frame)
+
+    # Save concatenated csv
+    data_frame = pd.concat(data_frames, axis=0, ignore_index=True)
+    data_frame.to_csv('dataset.csv', index=False, encoding='utf-8')
+    print('Saved CSV to ./dataset.csv')
+
 if __name__ == "__main__":
-    main()
+    dataset_dir_name = '3d-ken-burns-dataset'
+    dataset_path = './'
+    csv = False
+
+    for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ 'path=', 'csv' ])[0]:
+        if strOption == '--path' and strArgument != '':
+            dataset_path = strArgument # path to the datasets
+        if strOption == '--csv':
+            csv = True # Flag to create csv of downloaded datasets
+
+    if dataset_dir_name not in dataset_path:
+        dataset_path = os.path.join(dataset_path, dataset_dir_name)
+
+    main(dataset_path)
+
+    if csv:
+        create_csv(dataset_path)
