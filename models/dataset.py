Fix calculation of inverse depth.

models/dataset.py
@@ -58,10 +58,8 @@ class ImageDepthDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        zip_image_path = os.path.join(
-            self.dataset_path, self.dataset_frame.iloc[idx, 0])
-        zip_depth_path = os.path.join(
-            self.dataset_path, self.dataset_frame.iloc[idx, 1])
+        zip_image_path = os.path.join(self.dataset_path, self.dataset_frame.iloc[idx, 0])
+        zip_depth_path = os.path.join(self.dataset_path, self.dataset_frame.iloc[idx, 1])
         image_path = self.dataset_frame.iloc[idx, 2]
         depth_path = self.dataset_frame.iloc[idx, 3]
 
@@ -70,14 +68,20 @@ class ImageDepthDataset(Dataset):
         # Read image
         archive_image = ZipFile(zip_image_path, 'r')
         image_bytes = archive_image.read(image_path)
-        image = cv2.imdecode(np.frombuffer(
-            image_bytes, np.uint8), cv2.IMREAD_COLOR)
+        image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
 
         # Read depth
         archive_depth = ZipFile(zip_depth_path, 'r')
         depth_bytes = archive_depth.read(depth_path)
-        depth = cv2.imdecode(np.frombuffer(
-            depth_bytes, np.uint8), cv2.IMREAD_ANYDEPTH)
+        depth = cv2.imdecode(np.frombuffer(depth_bytes, np.uint8), cv2.IMREAD_ANYDEPTH)
+
+        # Convert 32bit depth values to float values
+        # TODO: Set baseline dynamically
+        baseline = 20
+        max_dim = max(image.shape[0], image.shape[1]) / 2
+        fltFov_ = fltFov / 2
+        focal = max_dim / np.tan(np.deg2rad(fltFov_))
+        depth = (focal * baseline) / (depth + 0.0000001)
 
         sample = {'image': image, 'depth': depth, 'fltFov': fltFov}
 

models/train_disparity_estimation.py
@@ -85,21 +85,6 @@ def loss_depth(output, target):
     )
 
 
-def get_inverse_depth(image, disparity, fltFov, baseline=20):
-    inverse_depth = disparity.clone()
-
-    # calculate focal length with formula: F = A/tan(a)
-    max_dim = max(image.shape[2], image.shape[3]) / 2
-    fltFov = fltFov / 2
-    focal = max_dim / np.tan(fltFov)
-
-    # calculate inverse depth with formula: (focal * baseline) / (0.0000001 + disparity)
-    for i in range(disparity.shape[0]):
-        inverse_depth[i,:,:,:] = (focal[i] * baseline) / (0.0000001 + disparity[i,:,:,:])
-
-    return inverse_depth
-
-
 # TODO: continue training from latest checkpoint
 # TODO: collect accuracy and loss for plots
 def train(args, model, semanticsModel, device, data_loader, optimizer, epoch):
@@ -119,14 +104,13 @@ def train(args, model, semanticsModel, device, data_loader, optimizer, epoch):
         progress = 100. * current_step / total_steps
 
         # forward pass through Semantics() network
-        semanticsOutput = semanticsModel(image)
+        with torch.no_grad():
+            semanticsOutput = semanticsModel(image)
         disparity = model(image, semanticsOutput)
-
-        # get inverse depth of disparity
-        inverse_depth = get_inverse_depth(image, disparity, fltFov)
+        disparity = torch.nn.functional.threshold(disparity, threshold=0.0, value=0.0)
 
         # calculate loss
-        loss = loss_depth(inverse_depth, depth).mean()
+        loss = loss_depth(disparity, depth).mean()
         loss.backward()
         optimizer.step()
 
@@ -152,12 +136,9 @@ def train(args, model, semanticsModel, device, data_loader, optimizer, epoch):
 
 
 def save_disparity(disparity, file_name):
-    multiplier = 255.0 / torch.max(disparity)
-    disparity_output = disparity * multiplier
-    disparity_output = disparity_output[0,0,:,:].detach().cpu().numpy()
-    cv2.imwrite(file_name, disparity_output)
-    # image_output = image[0,:,:,:].detach().cpu().numpy().transpose(1,2,0) * 255
-    # cv2.imwrite(f'./logs/{file_name}-image.jpg', image_output)
+    disparity_out = (disparity[0,0,:,:] / 20 * 255.0).clamp(0.0, 255.0).type(torch.uint8)
+    disparity_out = disparity_out.detach().cpu().numpy()
+    cv2.imwrite(file_name, disparity_out)
 
 
 def pad_current_step(max_steps, current_step):

models/transforms.py
@@ -15,16 +15,14 @@ class ToTensor(object):
         # numpy image: H x W x C
         # torch image: C X H X W
         # normalize image
-        image = torch.FloatTensor(np.ascontiguousarray(
-            image.transpose(2, 0, 1).astype(np.float32)) * (1.0 / 255.0)).cuda()
-        depth = torch.FloatTensor(np.ascontiguousarray(
-            depth[None, :, :].astype(np.float32))).cuda()
+        image = torch.FloatTensor(np.ascontiguousarray(image.transpose(2, 0, 1).astype(np.float32)) * (1.0 / 255.0)).cuda()
+        depth = torch.FloatTensor(np.ascontiguousarray(depth[None, :, :].astype(np.float32))).cuda()
 
         return {'image': image, 'depth': depth, 'fltFov': fltFov}
 
 
 class DownscaleDepth(object):
-    """Convert ndarrays in sample to Tensors."""
+    """Downscale target depth by factor 2"""
 
     def __call__(self, sample):
         image, depth, fltFov = sample['image'], sample['depth'], sample['fltFov']