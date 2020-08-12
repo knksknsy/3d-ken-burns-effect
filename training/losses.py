Fix loss calculation for inpainting training.

training/losses.py
@@ -29,42 +29,26 @@ def derivative_scale(tensor, h, device, norm=True):
     return torch.nn.functional.pad(diff_x, (0, 0, h, 0)), torch.nn.functional.pad(diff_y, (h, 0, 0, 0))
 
 
-def compute_loss_ord(disparity, target, mask):
-    L1Loss = torch.nn.L1Loss(reduction='sum')
-
-    loss = 0
-    N = torch.sum(mask)
-    
-    if N != 0:
-        loss = L1Loss(disparity * mask, target * mask) / N
-    return loss
+def compute_l1_loss(disparity, target):
+    L1Loss = torch.nn.L1Loss(reduction='mean')
+    return L1Loss(disparity, target)
 
 
-def compute_loss_grad(disparity, target, mask, device):        
+def compute_loss_grad(disparity, target, device):        
     scales = [2**i for i in range(4)]
-    MSELoss = torch.nn.MSELoss(reduction='sum')
+    MSELoss = torch.nn.MSELoss(reduction='mean')
 
     loss = 0
     for h in scales:
         g_h_disparity_x, g_h_disparity_y = derivative_scale(disparity, h, device, norm=True)
         g_h_target_x, g_h_target_y = derivative_scale(target, h, device, norm=True)
-        N = torch.sum(mask)
 
-        if N != 0:
-            loss += MSELoss(g_h_disparity_x * mask, g_h_target_x * mask) / N
-            loss += MSELoss(g_h_disparity_y * mask, g_h_target_y * mask) / N
+        loss += MSELoss(g_h_disparity_x, g_h_target_x)
+        loss += MSELoss(g_h_disparity_y, g_h_target_y)
 
     return loss
 
 
-def compute_loss_perception(output, target, device):
-    mask = torch.ones(output.shape).to(device)
-    MSELoss = torch.nn.MSELoss(reduction='sum')
-
-    loss = 0
-    N = torch.sum(mask)
-
-    if N != 0:
-        loss += (MSELoss(output * mask, target * mask) / N).pow(2)
-
-    return loss
+def compute_loss_perception(output, target):
+    MSELoss = torch.nn.MSELoss(reduction='mean')
+    return MSELoss(output, target)

training/train_disparity_estimation.py
@@ -1,7 +1,7 @@
 from disparity_estimation import Disparity, Semantics
 from transforms import ToTensor, DownscaleDepth, RandomRescaleCrop
 from dataset import ImageDepthDataset
-from losses import get_kernels, derivative_scale, compute_loss_ord, compute_loss_grad
+from losses import get_kernels, derivative_scale, compute_l1_loss, compute_loss_grad
 from utils import load_model, save_model, get_eta_string, save_log, pad_number
 
 import torch
@@ -37,9 +37,8 @@ def train(args, disparityModel, semanticsModel, data_loader, optimizer, schedule
         disparity = disparityModel(image, semanticsOutput)
 
         # reconstruction loss computation
-        mask = torch.ones(depth.shape).to(device)
-        loss_ord = compute_loss_ord(disparity, depth, mask)
-        loss_grad = compute_loss_grad(disparity, depth, mask, device)
+        loss_ord = compute_l1_loss(disparity, depth)
+        loss_grad = compute_loss_grad(disparity, depth, device)
 
         # # loss weights computation
         # beta = 0.015

training/train_disparity_refinement.py
@@ -1,7 +1,7 @@
 from disparity_refinement import Refine
 from transforms import ToTensor, DownscaleDepth
 from dataset import ImageDepthDataset
-from losses import get_kernels, derivative_scale, compute_loss_ord, compute_loss_grad
+from losses import get_kernels, derivative_scale, compute_l1_loss, compute_loss_grad
 from utils import load_model, save_model, get_eta_string, save_log, pad_number
 
 import torch
@@ -33,9 +33,8 @@ def train(args, refinementModel, data_loader, optimizer, scheduler, epoch, iter_
         disparity_refined = refinementModel(image, depth_adjusted)
 
         # reconstruction loss computation
-        mask = torch.ones(depth.shape).to(device)
-        loss_ord = compute_loss_ord(disparity_refined, depth, mask)
-        loss_grad = compute_loss_grad(disparity_refined, depth, mask, device)
+        loss_ord = compute_l1_loss(disparity_refined, depth)
+        loss_grad = compute_loss_grad(disparity_refined, depth, device)
 
         loss_depth = 0.0001 * loss_ord + loss_grad
         loss_depth.backward()

training/train_pointcloud_inpainting.py
@@ -1,7 +1,7 @@
 from pointcloud_inpainting import Inpaint
 from transforms import ToTensor, RandomWarp
 from dataset import ImageDepthDataset
-from losses import get_kernels, derivative_scale, compute_loss_ord, compute_loss_grad, compute_loss_perception
+from losses import get_kernels, derivative_scale, compute_l1_loss, compute_loss_grad, compute_loss_perception
 from utils import load_model, save_model, get_eta_string, save_log, pad_number
 
 import torch
@@ -30,19 +30,18 @@ def train(args, inpaintModel, vggModelRelu4, data_loader, optimizer, scheduler, 
         optimizer.zero_grad()
         
         inpaint_color, inpaint_depth = inpaintModel(image_masked, image_gt, depth_masked, depth_gt)
-        mask = torch.ones(depth_gt.shape).to(device)
 
         # color inpainting loss cumputation
-        loss_color = compute_loss_ord(inpaint_color, image_gt, mask)
+        loss_color = compute_l1_loss(inpaint_color, image_gt)
 
         with torch.no_grad(): # disable calculation of gradients
             vgg_inpaint_color, vgg_image_gt = vggModelRelu4(inpaint_color), vggModelRelu4(image_gt)
 
-        loss_perception = compute_loss_perception(vgg_inpaint_color, vgg_image_gt, device)
+        loss_perception = compute_loss_perception(vgg_inpaint_color, vgg_image_gt)
         
         # depth inpainting loss computation
-        loss_ord = compute_loss_ord(inpaint_depth, depth_gt, mask)
-        loss_grad = compute_loss_grad(inpaint_depth, depth_gt, mask, device)
+        loss_ord = compute_l1_loss(inpaint_depth, depth_gt)
+        loss_grad = compute_loss_grad(inpaint_depth, depth_gt, device)
         loss_depth = 0.0001 * loss_ord + loss_grad
 
         # combined loss computation